37210158ac41d9a633e1a71a5c09ba2357502bb9
ABBAUMAR24 <abbaumar360@gmail.com>
2024-01-30 01:35:00 +0100

    just playing with api

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
 import  React from "react";
-import { StyleSheet, Text, SafeAreaView , TouchableOpacity} from 'react-native';
-import { useState , } from "react";
+import { StyleSheet, Text, SafeAreaView , Button, TextInput, View, TouchableOpacity, FlatList} from 'react-native';
+import { useState , useEffect} from "react";
 import tw from 'twrnc';
 
 
 export default function App() {
+  const [data, setdata] = useState([])
+  
+
+const fetchdata = async ( ) => {
+ res = await fetch (  `https://jsonplaceholder.typicode.com/posts/1`)
+ res = await res.json()
+ setdata(res)
+
+}
+
+useEffect(() => {
+  fetchdata();
+},[])
+
+
   const [name, setname] = useState('')
+   const Del = () => {
+    setname(' ')
+   }
+  
   return (
-    <SafeAreaView >
-       <TouchableOpacity>
+    <SafeAreaView style={tw `mx-10 my-20 `}>
+      <View >
+        <TextInput
+        value={name}
+        placeholder='text here'
+        onChangeText={(setname)}
+        />
+
+      <Text style={tw ``}>text this text is at the top {name} </Text>
+      <TouchableOpacity onPress={(Del)}>
+        
+        <Text style={tw `bg-red-400`}>clear text</Text>
+      </TouchableOpacity>
       
-        <Text>me</Text>
-       </TouchableOpacity>
-      <Text style={tw `text-black ${!name  } : text-red-400`}>hello {name}</Text>
       <StatusBar style="auto" />
+      <FlatList
+      data={data}
+      keyExtractor={({id}) => id}
+      renderItem={({item}) =>{
+        <Text>{console.log(item)}</Text>
+      }}
+      />
+    </View>
     </SafeAreaView>
+    
   );
 }
 
