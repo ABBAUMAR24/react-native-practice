56668662978507f1a0e10094178e4e05feb1e066
ABBAUMAR24 <abbaumar360@gmail.com>
2024-02-01 21:38:54 +0100

    nothing much actully

--- App.js
+++ App.js
@@ -1,27 +1,47 @@
-import { StyleSheet, Text, TextInput,  View, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, TextInput,  View, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import tw  from "twrnc";
 
 const App = () => {
   return (
     <View style={tw `mt-10`}>
-      <Text style={tw `text-3xl  font-semibold text-center `}>Super World</Text>
+      <Text style={[tw `text-3xl  font-semibold text-center `, styles.textt]}>Super World</Text>
       <TextInput
-      style={tw `block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mt-5`}
+      style={tw ` w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mt-5`}
       placeholder='search for pdf'   
       />
        <View >
-        <TouchableOpacity style={tw `grid flex-1 w-auto`}>
+        <TouchableOpacity style={ [styles.container , tw `pt-5  `]}>   
           
           <Text>my book 1</Text>
           <Text>my book 2</Text>
           <Text>my book 3</Text>
         </TouchableOpacity>
        </View>
+       <View>
+          <Image
+          source={{
+          uri: 'https://reactnative.dev/img/tiny_logo.png',
+        }}          
+         />
+        </View>  
     </View>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    padding: 10   
+    
+  },  
+  textt: {
+    fontSize: 30,
+    fontWeight: 'bold'
+    
+  
+  }
+})
